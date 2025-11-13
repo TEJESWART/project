64c8051eceed87d0645d2a467cb4e82aa816b0bc
Tejeswar T <tejeswar@example.com>
2025-11-13 12:37:27 +0530

    fixed user creation

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -7,12 +7,12 @@ export const inngest = new Inngest({ id: "project-management" });
 const syncUserCreation = inngest.createFunction(
     {id: 'sync-user-from-clerk'},
     {event: 'clerk/user.created'},
-    async ({ event })=>{
+    async ({ event })=>{    
         const {data} = event
         await prisma.user.create({
             data: {
                 id: data.id,
-                email: data?.email_address[0]?.email_address,
+                email: data?.email_addresses[0]?.email_addres,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url,
             }
@@ -52,7 +52,7 @@ const syncUserUpdation = inngest.createFunction(
             },
             data: {
                 id: data.id,
-                email: data?.email_address[0]?.email_address,
+                email: data?.email_addresses[0]?.email_addres,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url,
             }
